91f8752126c97d339849a6c2145217fe0fee5276
shustova <shustova@Belkanton.local>
2021-09-28 11:47:02 +0300

    aloha dogapi fix

--- services/dogapi.js
+++ services/dogapi.js
@@ -1,20 +1,17 @@
 const { default: axios } = require('axios');
 const dogApi = 'https://dog.ceo/api/breeds/image/random/50';
+const count = 100;
 
 const getDataFromDogsApi = async () => {
-  let requests = [];
-  for (let i = 0; i < 2; i++) {
-    requests.push(axios.get(dogApi));
-  }
-  return Promise.all(requests);
+  return Promise.resolve(axios.get(dogApi));
 };
 
 const prepareDataFromDogApi = async (data) => {
   const result = [];
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].data.message.length; j++) {
-      result.push(breedParseUrl(data[i].data.message[j]));
-    }
+  for (let i = 0; i < data.data.message.length; i++) {
+    // for (let j = 0; j < data[i].data.message.length; j++) {
+      result.push(breedParseUrl(data.data.message[i]));
+    // }
   }
   return result;
 };
@@ -29,9 +26,33 @@ const breedParseUrl = (url) => {
   };
 };
 
+const prepareWithoutDoubles = (data, newData) => {
+    const result = newData.filter(function (curr, index) {
+        let flag = data.find(function (curr2) {
+            if (curr.breed == curr2.breed) {
+                return true;
+            }
+        });
+        if (!flag && (data.length + (index + 1)) <= count) {
+            return curr;
+        }
+    });
+    return result;
+};
+
+const getData = async arr => {
+  const dataFromApi = await getDataFromDogsApi(); // достаю рандомные 50
+  const data = await prepareDataFromDogApi(dataFromApi); // привожу их в порядок, вид, которые мне нужен
+  const checkedData = prepareWithoutDoubles(arr, data); // проверяю повторки
+  let result = arr.concat(checkedData); // соединяю с общими данными (изначально пустой массив)
+  if (result.length < count) {
+    result = await getData(result); // если меньше ста - рекурсия
+  }
+  return result;
+};
+
 const get = async () => {
-  const dataFromApi = await getDataFromDogsApi();
-  const data = await prepareDataFromDogApi(dataFromApi);
+  const data = await getData([]);
   return data;
 };
 
